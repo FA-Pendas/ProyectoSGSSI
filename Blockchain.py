ced90bb213deae947452d853c7bd6920cb850861
FA-Pendas <pendasendika@gmail.com>
2023-12-30 16:59:50 +0100

    avances en la clase blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -1,18 +1,113 @@
+# importar librerias
+import hashlib
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import serialization
+import base64
+import time
+
 class Blockchain():
 
-    # importar librerias
-    import hashlib
-    from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives import hashes
-    from cryptography.hazmat.primitives.asymmetric import rsa, padding
-    from cryptography.exceptions import InvalidSignature
-    import base64
-
     def __init__(self) -> None:
-        self.cadenaDeBloques = [] # Lista de bloques
-        self.trasacciones = {} # Diccionario de transacciones existentes {hash:transaccion} 
+        self.bloques = {} # Diccionario de bloques {hash:bloque}
+        self.cadenaDeBloques = [] # Lista de hashes de bloques
+        self.trasacciones = {} # Diccionario de transacciones existentes {hash:transaccion}
+        self.trasaccionesNoQuemadas = {} # Diccionario de transacciones no quemadas {hash:transaccion} 
         self.mempool = [] # Lista de transacciones pendientes
         self.nodos = set() # Conjunto de nodos conectados
+
+    def hola(self):
+        print("Hola")
+        return None
+
+    def crear_direccion(self, public_key:str):
+        return hash(public_key)
+
+    def crear_mensaje(self, emisor:str, receptor:str, cantidad:int):
+        return f"{emisor} {receptor} {cantidad}"
+
+    def firmar(self, private_key:str, mensaje:str, contrasena:str = None):
+        private_key_from_str = serialization.load_pem_private_key(
+            private_key.encode('utf-8'),
+            password=contrasena,
+            backend=default_backend()
+        )
+        signature = private_key.sign(
+            mensaje,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return base64.b64encode(signature).decode('utf-8')
+
+    def crear_transaccion(self, public_key:str, private_key:str, mensaje:str, contrasena:str = None):
+        
+        #Obtenemos el receptor, emisor y cantidad del mensaje
+        emisor, receptor, cantidad = mensaje.split(" ")
+
+        #Convertimos la clave publica a un objeto de tipo public_key
+        public_key_from_str = serialization.load_pem_public_key(
+            public_key.encode('utf-8'),
+            backend=default_backend()
+        )
+
+        #Firmamos el mensaje
+        firma = self.firmar(private_key, mensaje, contrasena)
+
+        #Devolvemos la transaccion
+        return { #TIENE QUE TENER ESTE ORDEN
+            "time-stamp": time.time(), # O int(time.time())
+            "emisor": emisor,
+            "receptor": receptor,
+            "cantidad": float(cantidad),
+            "firma": firma,
+            "clave-publica": public_key_from_str
+        }
+
+    def anadir_transaccion(self, transaccion):
+        # Añadir la transacción al mempool
+        self.mempool.append(transaccion)
+        #TODO: Cuando se añada a la cadena, quitarla del mempool y hacerle el hash en modo string para que el hash sea la clave en el diccionario de transacciones
+
+    def obtener_hash_de_transaccion(self, transaccion):
+        return self.hash(str(transaccion))
+
+    def obtener_dificultad(self):
+        return self.ultimo_bloque().dificultad # TODO: esto hay que cambiarlo, que cambie cada 10 bloques?
+
+    def crear_bloque(self, hash_anterior):#, transacciones):
+        # Crear un bloque
+        # añadir todas las transacciones del mempool al bloque (todavia no borrarlas del mempool)
+        transacciones = []
+        for i in self.mempool:
+            transacciones.append(self.obtener_hash_de_transaccion(i))
+
+        bloque = {
+            "indice": self.ultimo_bloque().indice + 1,
+            "transacciones": transacciones,
+            "dificultad": self.obtener_dificultad(),
+            "hash-anterior": self.ultimo_bloque().hash
+            #TIENE QUE SER EN ESTE ORDEN
+            # "time-stamp": 1357425863,
+            # "nonce": 0,
+            # "hash": "0x356435345"
+        }
+        pass
+
+    def ultimo_bloque(self):
+        return self.bloques[self.cadenaDeBloques[-1]]
+    
+    def minar(self, bloque):
+        # Una vez el bloque es valido, se mina
+        pass
+
+    def hash(self, string:str):
+        # Aplica el algoitmo sha256 a un string puede ser (clave publica, bloque, transaccion)
+        return hashlib.sha256(string.encode('utf-8')).hexdigest()
 
     def validar_transaccion(self, transaccion):
 
@@ -33,10 +128,41 @@ class Blockchain():
         for transaccion in bloque.transacciones:
             if not self.validar_transaccion(transaccion):
                 valido = False
-        # Validar el bloque
-        # Si es válido, agregarlo a la cadena
-        # Si no es válido, descartarlo
+                break
+        # CALCULAR EL HASH DEL BLOQUE Y COMPARAR QUE SOBREPASA LA DIFICULTAD
+        # COMPROBAR QUE LA DIFICULTAD DEL BLOQUE ES LA CORRECTA?
 
         # Si el bloque es válido, agregarlo a la cadena
-        self.cadena.append(bloque)
+        if valido:
+            h = self.obtener_hash_de_bloque(bloque)
+            self.cadenaDeBloques.append(h)
+            self.bloques[h] = bloque
+        else:
+            print("Bloque no válido")
 
+
+    def obtener_hash_de_bloque(self, bloque):
+        return self.hash(str(bloque))
+
+    def agregar_nodo(self, ipNodo):
+        self.nodos.add(ipNodo)
+
+    def agregar_transaccion(self, transaccion):
+        if self.validar_transaccion(transaccion):
+            self.mempool.append(transaccion)
+        else:
+            print("Transacción no válida")
+
+    def reemplazar_cadena(self, nueva_cadena):
+        # Reemplazar la cadena actual por la nueva en caso de que sea válida y mas larga (si es igual nos quedamos con la nuestra)
+        pass
+
+    def propagar_cadena(self, cadena):
+        for nodo in self.nodos:
+            # Enviar la cadena a todos los nodos
+            pass
+
+    def propagar_transaccion(self, transaccion):
+        for nodo in self.nodos:
+            # Enviar la transacción a todos los nodos
+            pass
--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,23 @@
 #La b lo vuelve en bytes
-a = 'a'
-a = bytes(a, 'utf-8')
-b = b"a"
-print(a[0]>>6, b[0]>>6)
+# a = 'a'
+# a = bytes(a, 'utf-8')
+# b = b"a"
+# print(a[0]>>6, b[0]>>6)
+
+
+# from Blockchain import Blockchain
+
+# a = Blockchain()
+# a.hola()
+
+
+# emisor = "emitir"
+# receptor = "receptor"
+# cantidad = 10
+# print(f"{emisor} {receptor} {cantidad}")
+
+
+import time
+print(time.time())
+time.sleep(1)
+print(time.time())
