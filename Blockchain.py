5b7b8529dfe92e2e76a016355d268f9acee91e15
Endika Alonso <ealonso073@ikasle.ehu.eus>
2024-01-09 16:08:54 +0100

    commit antes del cambio de planteamiento en las transacciones

--- Blockchain.py
+++ Blockchain.py
@@ -7,28 +7,42 @@ from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import serialization
 import base64
 import time
+import threading
 
 class Blockchain():
 
-    def __init__(self) -> None:
+    def __init__(self, direccion = "0x0000000000000000000000000000000000000000000000000000000000000000") -> None:
+        
+        self.direccion = direccion # Direccion del nodo
+
         self.bloques = {} # Diccionario de bloques {hash:bloque}
         self.cadenaDeBloques = [] # Lista de hashes de bloques
+
+        self.trasaccionesDeEnvio = {} # Diccionario de transacciones de envio #VERSION1.1
+#       {hash:{
+#           "enviado": hash       
+#           "sobras": hash
+#           "comision": hash
+#       }}
+
         self.trasacciones = {} # Diccionario de transacciones existentes {hash:transaccion}
         self.trasaccionesNoQuemadas = {} # Diccionario de transacciones no quemadas {hash:transaccion} 
-        self.mempool = [] # Lista de listas de transacciones pendientes
+        
+        self.mempool = [] # Lista de listas de transacciones de envio pendientes
+        
         self.nodos = set() # Conjunto de nodos conectados
 
     def hola(self):
         print("Hola")
         return None
 
-    def crear_direccion(self, public_key:str):
+    def crear_direccion(self, public_key:str):#VERSION1.0
         return hash(public_key)
 
-    def crear_mensaje(self, emisor:str, receptor:str, cantidad:int):
+    def crear_mensaje(self, emisor:str, receptor:str, cantidad:int):#VERSION1.0
         return f"{emisor} {receptor} {cantidad}"
 
-    def firmar(self, private_key:str, mensaje:str, contrasena:str = None):
+    def firmar(self, private_key:str, mensaje:str, contrasena:str = None):#VERSION1.0
         private_key_from_str = serialization.load_pem_private_key(
             private_key.encode('utf-8'),
             password=contrasena,
@@ -44,7 +58,7 @@ class Blockchain():
         )
         return base64.b64encode(signature).decode('utf-8')
 
-    def crear_transaccion(self, public_key:str, private_key:str, mensaje:str, contrasena:str = None):
+    def crear_transaccion(self, public_key:str, private_key:str, mensaje:str, contrasena:str = None):#VERSION1.0
         
         #Obtenemos el receptor, emisor y cantidad del mensaje
         emisor, receptor, cantidad = mensaje.split(" ")
@@ -68,27 +82,46 @@ class Blockchain():
             "clave-publica": public_key_from_str
         }
 
+    def validar_transaccion_no_quemada(self, transaccion, emisor):#VERSION1.0
+        return self.trasaccionesNoQuemadas[transaccion].receptor == emisor
+
     # TODO: SE AÑADEN MAS DE UNA TRANSACCION A LA VEZ (son dependientes entre ellas)
     # TODO: Que en la mempool haya listas con transacciones que se tienen que minar juntas
-    def anadir_transaccion(self, transaccion, transaccionesAQuemar):
+    # TODO: El restante se le pasa al emisor
+    # LAS TRANSACCIONES QUE SON DE UNA PERSONA A ELLA MISMA NO SE TIENEN QUE FIRMAR, SON RESIDUOS Y SON VALIDOS
+    # TODO: PARA SIMPLIFICAR QUE LA CANTIDAD QUE SE LE PASA A LA GENTE SEA LA TOTAL DE LAS TRANSACCIONES QUE SE VAN A QUEMAR
+    def anadir_transaccion(self, transaccion: dict, transaccionesAQuemar: list):#VERSION1.0
+        cantidad = 0
+        for i in transaccionesAQuemar:
+            if self.validar_transaccion_no_quemada(i, transaccion.emisor):
+                cantidad += self.trasaccionesNoQuemadas[i].cantidad
+        
+        if cantidad < transaccion.cantidad:
+            return False
+        
+        #Eliminar las transacciones a quemar de la lista de transacciones no quemadas
+        for i in transaccionesAQuemar:
+            if self.validar_transaccion_no_quemada(i, transaccion.emisor):
+                self.trasaccionesNoQuemadas.pop(i)
         # Transacciones a quemar es una lista de transacciones que se van a quemar en esta transaccion
         # El sobrante se le pasara a la direccion del emisor
         if self.validar_transaccion(transaccion):
             self.mempool.append(transaccion)
             #TODO: Cuando se añada a la cadena, quitarla del mempool y hacerle el hash en modo string para que el hash sea la clave en el diccionario de transacciones
 
-    def obtener_hash_de_transaccion(self, transaccion):
+    def obtener_hash_de_transaccion(self, transaccion):#VERSION1.0
         return self.hash(str(transaccion))
 
-    def obtener_dificultad(self):
+    def obtener_dificultad(self):#VERSION1.0
         return self.ultimo_bloque().dificultad # TODO: esto hay que cambiarlo, que cambie cada 10 bloques?
 
-    def crear_bloque(self, hash_anterior):#, transacciones):
+    def crear_bloque(self, hash_anterior):#, transacciones):#VERSION1.0
         # Crear un bloque
         # añadir todas las transacciones del mempool al bloque (todavia no borrarlas del mempool)
         transacciones = []
         for i in self.mempool: # En la mempool solo hay transacciones validas
-            transacciones.append(self.obtener_hash_de_transaccion(i))
+            for j in i:
+                transacciones.append(self.obtener_hash_de_transaccion(j))
 
         bloque = {
             "indice": self.ultimo_bloque().indice + 1,
@@ -102,18 +135,18 @@ class Blockchain():
         }
         pass
 
-    def ultimo_bloque(self):
+    def ultimo_bloque(self):#VERSION1.0
         return self.bloques[self.cadenaDeBloques[-1]]
     
-    def minar(self, bloque):
+    def minar(self, bloque):#VERSION1.0
         # Una vez el bloque es valido, se mina
         pass
 
-    def hash(self, string:str):
+    def hash(self, string:str):#VERSION1.0
         # Aplica el algoitmo sha256 a un string puede ser (clave publica, bloque, transaccion)
         return hashlib.sha256(string.encode('utf-8')).hexdigest()
 
-    def validar_transaccion(self, transaccion, esHash=False):
+    def validar_transaccion(self, transaccion, esHash=False):#VERSION1.0
 
         if esHash:
             transaccion = self.trasacciones[transaccion]
@@ -146,7 +179,7 @@ class Blockchain():
         
         return esValida
 
-    def validar_bloque(self, bloque, hash_anterior):
+    def validar_bloque(self, bloque, hash_anterior):#VERSION1.0
         valido = True
         if bloque.hash_anterior != hash_anterior:
             valido = False
@@ -168,28 +201,28 @@ class Blockchain():
             print("Bloque no válido")
 
 
-    def obtener_hash_de_bloque(self, bloque):
+    def obtener_hash_de_bloque(self, bloque):#VERSION1.0
         return self.hash(str(bloque))
 
-    def agregar_nodo(self, ipNodo):
+    def agregar_nodo(self, ipNodo):#VERSION1.0
         self.nodos.add(ipNodo)
 
-    def agregar_transaccion(self, transaccion):#TODO: Borrar esta funcion
+    def agregar_transaccion(self, transaccion):#TODO: Borrar esta funcion#VERSION1.0
         if self.validar_transaccion(transaccion):
             self.mempool.append(transaccion)
         else:
             print("Transacción no válida")
 
-    def reemplazar_cadena(self, nueva_cadena):
+    def reemplazar_cadena(self, nueva_cadena):#VERSION1.0
         # Reemplazar la cadena actual por la nueva en caso de que sea válida y mas larga (si es igual nos quedamos con la nuestra)
         pass
 
-    def propagar_cadena(self, cadena):
+    def propagar_cadena(self, cadena):#VERSION1.0
         for nodo in self.nodos:
             # Enviar la cadena a todos los nodos
             pass
 
-    def propagar_transaccion(self, transaccion):
+    def propagar_transaccion(self, transaccion):#VERSION1.0
         for nodo in self.nodos:
             # Enviar la transacción a todos los nodos
             pass
--- pruebas.py
+++ pruebas.py
@@ -22,3 +22,58 @@
 # time.sleep(1)
 # print(time.time())
 
+######
+# semaforo = False
+
+# def bucle():
+#     print("Entre al bucle")
+#     global semaforo
+#     while True:
+#         if semaforo:
+#             break
+#     semaforo = False
+#     print("Sali del bucle")
+
+# def cambiar():
+#     global semaforo
+#     semaforo = True
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     bucle()
+#     return "<p>Hello, World!</p>"
+
+# @app.route("/stop")
+# def parar():
+#     cambiar()
+#     return "<p>Se Supone que se paro</p>"
+
+import threading
+import time
+
+class MiClase:
+
+    def __init__(self):
+        self.semaforo = False
+        self.hilo = threading.Thread(target=self.bucle)
+        self.hilo.start()
+
+    def bucle(self):
+        print("Entre al bucle")
+        while True:
+            if self.semaforo:
+                break
+        self.semaforo = False
+        print("Sali del bucle")
+
+    def cambiar(self):
+        self.semaforo = True
+
+a = MiClase()
+print("Hola")
+time.sleep(1)
+a.cambiar()
